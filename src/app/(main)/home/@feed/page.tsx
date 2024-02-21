0b63ab5261d3347bb016e9cc775f54e3ccd82475
Alexander Pastor <apastor@umd.edu>
2024-02-20 22:02:05 -0500

    removed unnecessary supabase api file

--- src/app/(main)/home/@feed/page.tsx
+++ src/app/(main)/home/@feed/page.tsx
@@ -1,7 +1,9 @@
 import Post from "@/components/Post";
+import { getNextPostsPage } from "@/lib/actions";
 
 const ITEMS_PER_PAGE = 10;
-export default function FeedPage() {
+export default async function FeedPage() {
+  const posts = await getNextPostsPage(1, 10);
   return (
     <div>
       <p className="text-center">feed page</p>
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,14 +1,15 @@
 "use server";
 
 import { redirect } from "next/navigation";
-import {
-  createNewPost,
-  createNewPostProps,
-  signInWithEmail,
-  signUpNewUser,
-} from "./supabase/supabaseApi";
+import { cookies } from "next/headers";
+import { createClient } from "@/utils/supabase/server";
+import { createPostReturn } from "./types";
+import { PostgrestError } from "@supabase/supabase-js";
 
 export async function signUpUser(prevState: any, formData: FormData) {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
   let email = formData.get("email");
   let password = formData.get("password");
   if (!email || !password) {
@@ -18,19 +19,15 @@ export async function signUpUser(prevState: any, formData: FormData) {
   email = email.toString();
   password = password.toString();
 
-  try {
-    const res = await signUpNewUser({ email, password });
-  } catch (error) {
-    if (error instanceof Error) {
-      return {
-        error: error.message,
-      };
-    }
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
 
-    return {
-      error: "something went wrong while signing up",
-    };
+  if (error) {
+    return { error: error.message };
   }
+
   return {
     message: "success",
     error: null,
@@ -38,6 +35,9 @@ export async function signUpUser(prevState: any, formData: FormData) {
 }
 
 export async function emailLogin(prevState: any, formData: FormData) {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
   let email = formData.get("email");
   let password = formData.get("password");
   if (!email || !password) {
@@ -52,27 +52,20 @@ export async function emailLogin(prevState: any, formData: FormData) {
   email = email.toString();
   password = password.toString();
 
-  try {
-    const res = await signInWithEmail({ email, password });
-  } catch (error) {
-    if (error instanceof Error) {
-      return {
-        message: "",
-        generalError: error.message,
-        emailError: null,
-        passwordError: null,
-      };
-    }
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
 
+  if (error) {
     return {
       message: "",
-      generalError: "something went wrong while signing in",
+      generalError: error,
       emailError: null,
       passwordError: null,
     };
   }
-  // console.log("redirecting");
-  // redirect("/home");
+
   return {
     message: "success",
     generalError: null,
@@ -81,18 +74,157 @@ export async function emailLogin(prevState: any, formData: FormData) {
   };
 }
 
+export async function getUser() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) {
+    throw new Error(error.message);
+  }
+  if (!data.user) {
+    throw new Error("UNABLE TO FETCH USER DATA");
+  }
+
+  return data.user;
+}
+
 export async function createPost(prevState: any, formData: FormData) {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const user_id = (await supabase.auth.getUser()).data.user?.id;
+
+  if (!user_id) {
+    throw new Error("user id not found");
+  }
+
   const content = formData.get("content") as FormDataEntryValue;
+
   if (!content) {
     return {};
   }
 
   const image = formData.get("post_image");
 
-  const data = await createNewPost({
-    content: content.toString(),
-    image,
+  //CREATE POST
+  const {
+    data,
+    error,
+  }: { data: createPostReturn[] | null; error: PostgrestError | null } =
+    await supabase
+      .from("posts")
+      .insert([{ content, user_id, image_path: null }])
+      .select();
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  if (!data || !data[0]) {
+    throw new Error("unable to fetch data");
+  }
+
+  if (!data[0].id) {
+    throw new Error("record id is undefined");
+  }
+
+  //UPLOAD IMAGE IF IT EXISTS
+  const imageUrl = await uploadPostImage({
+    file: image as File,
+    user_id,
+    post_id: data[0].id,
   });
 
-  return {};
+  return { message: "success" };
+}
+
+export async function uploadPostImage({
+  file,
+  user_id,
+  post_id,
+}: {
+  file: File;
+  user_id: string;
+  post_id: number;
+}) {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  console.log("image file", file);
+
+  if (!file.name || !file.size) {
+    return;
+  }
+
+  const fileExtension = file.name.split(".")[1];
+
+  // Upload file using standard upload
+  const { data, error } = await supabase.storage
+    .from("post_images")
+    .upload(
+      `${user_id}/posts/${String(post_id)}/post-image.${fileExtension}`,
+      file,
+      {
+        contentType: "image/*",
+      }
+    );
+
+  if (error) {
+    // Handle error
+    throw new Error(error.message);
+  }
+
+  if (!data) {
+    throw new Error("image data is null");
+  }
+
+  //UPDATE IMAGE URL IN POSTS TABLE
+  const { error: updateError } = await supabase
+    .from("posts")
+    .update({ image_path: data.path })
+    .eq("id", post_id);
+
+  if (updateError) {
+    throw new Error(updateError.message);
+  }
+
+  console.log("IMAGE PATH SUPABASE", data.path);
+  return data.path;
+}
+
+export async function getNextPostsPage(
+  currentPage: number,
+  itemsPerPage: number
+) {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const start = currentPage * itemsPerPage - itemsPerPage;
+  const end = start + itemsPerPage - 1;
+  const { data, error } = await supabase
+    .from("posts")
+    .select("*")
+    .range(start, end);
+  console.log("post data", data);
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+}
+
+export async function downloadPostImage(postId: string) {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data, error } = await supabase.storage
+    .from("post_images")
+    .download("public/avatar1.png");
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
 }
